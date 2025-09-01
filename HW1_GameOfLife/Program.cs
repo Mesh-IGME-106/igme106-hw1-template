49fafb5898cc955eced65488ab7fd59d3d71a69d
Erika Mesh <esmvcs@rit.edu>
2025-09-01 11:26:41 -0400

    HW 1 starter project

---
+++ HW1_GameOfLife/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace HW1_GameOfLife
+{
+    /// <summary>
+    /// YOUR NAME
+    /// 
+    /// IGME-106 - HW 1: Game of Life
+    /// https://docs.google.com/document/d/13zQjG7a25GUA1Oh6pwQXUtF6lJTJscWuuL-wwnMBSIo/edit?usp=sharing
+    /// 
+    /// GitHub Repository Link: 
+    /// 
+    /// Release Notes: 
+    /// </summary>
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+        }
+
+    }
+}
